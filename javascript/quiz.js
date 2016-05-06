92187cc407f2defdc2e0f59408ec11d0cece8a41
Russell Ries <riesru@gmail.com>
2016-05-06 15:56:46 -0500

    add .carCard & .about styling

--- javascript/quiz.js
+++ javascript/quiz.js
@@ -5,7 +5,7 @@ var CarLot = (function(aug) {
   aug.populatePage = function(inventory) {
     // Loop over the inventory and populate the page
     for (var i = 0; i < inventory.length; i++) {
-      cardOutput.innerHTML += `<div class="col-md-4"><div style="border: 3px solid ${inventory[i].color}"><h2>${inventory[i].make}</h2><h5>${inventory[i].model}</h5><h6>${inventory[i].year}</h6><p>Price: $${inventory[i].price}</p><p>Color: ${inventory[i].color}</p><p class="about">${inventory[i].description}</p></div></div>`;
+      cardOutput.innerHTML += `<div class="col-md-4"><div class="carCard" style="border: 3px solid ${inventory[i].color}"><h2>${inventory[i].make}` + " " + `${inventory[i].model}</h2><h4>${inventory[i].year}</h4><p>Price: $${inventory[i].price}</p><p>Color: ${inventory[i].color}</p><p class="about">${inventory[i].description}</p></div></div>`;
     }
 
     // Now that the DOM is loaded, establish all the event listeners needed
